43a16c4d2790bf0ac93e5b874c9654b036e0554a
Bonnie <moller.bon@gmail.com>
2024-04-08 11:10:53 +0200

    check

--- src/Components/Checkbox/Checkbox.tsx
+++ src/Components/Checkbox/Checkbox.tsx
@@ -7,12 +7,12 @@ type CheckProps = {
 
 const Checkbox = ({title, big}: CheckProps) => {
   return (
-    <>
-        <label htmlFor="check" className={big ? "big card Checkbox" : "Checkbox"}>
+    <section className="checkbox">
+        <label htmlFor="check" className={big ? "big checkbox__label" : "checkbox__label"}>
             <input type="checkbox" name="checkbox"/>
             {title}
         </label>
-    </>
+    </section>
   )
 }
 
