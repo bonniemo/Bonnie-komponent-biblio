c80fa2952d5229000725b06075668a890e1d8e82
Bonnie <moller.bon@gmail.com>
2024-04-09 15:53:10 +0200

    todo with progressbar and confetti yaaaaay

--- src/Components/Todo/Todo.tsx
+++ src/Components/Todo/Todo.tsx
@@ -1,27 +1,56 @@
-
-import Checkbox from '../Checkbox/Checkbox';
-import ProgressBar from '../ProgressBar/ProgressBar';
+import { useState } from "react";
+import Checkbox from "../Checkbox/Checkbox";
+import ProgressBar from "../ProgressBar/ProgressBar";
+import confetti from "https://esm.run/canvas-confetti@1";
 
 const Todo = () => {
-    let todos = [
-        "Finish homework",
-        "Go grocery shopping",
-        "Call mom",
-        "Workout for 30 minutes",
-        "Read a chapter of a book",
-      ];
+  const [todos, setTodos] = useState([
+    { title: "Finish homework", isChecked: false },
+    { title: "Go grocery shopping", isChecked: false },
+    { title: "Call mom", isChecked: false },
+    { title: "Workout for 30 minutes", isChecked: false },
+    { title: "Read a chapter of a book", isChecked: false },
+    { title: "Call mom", isChecked: false },
+    { title: "Workout for 30 minutes", isChecked: false },
+    { title: "Read a chapter of a book", isChecked: false },
+    { title: "Read a chapter of a book", isChecked: false },
+  ]);
+  const [progress, setProgress] = useState(0);
+
+  const handleCheckboxChange = (index: number) => {
+    todos[index].isChecked = !todos[index].isChecked;
+    setTodos([...todos]);
+
+    const checkedCount = todos.filter((todo) => todo.isChecked).length;
+    const newProgress = Math.floor((checkedCount / todos.length) * 100);
+    setProgress(newProgress);
+
+    if (todos[index].isChecked) {
+        // Trigger confetti
+        confetti({
+          particleCount: 1500,
+          spread: 360,
+        });
+      }
+  };
 
   return (
     <>
-    <ProgressBar title="Progress" percentage={25} color={"green"} /> 
-    <article className="todo">
+      <ProgressBar title="Progress" percentage={progress} color={"green"} />
+      <article className="todo">
         <h2>My Todos:</h2>
         {todos.map((todo, index) => (
-          <Checkbox key={index} title={todo} big={true} />
+          <Checkbox
+            key={index}
+            title={todo.title}
+            isChecked={todo.isChecked}
+            big={true}
+            onChange={() => handleCheckboxChange(index)}
+          />
         ))}
       </article>
     </>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
